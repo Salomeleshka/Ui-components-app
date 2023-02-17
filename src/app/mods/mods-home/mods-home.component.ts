74ad82499af289b3dc09a92e8367b9dd780143cc
Salomeleshka <salomeleshka@gmail.com>
2023-02-17 16:56:15 +0100

    reusable components

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen: boolean = false;
+  items: any = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue becuse it is!' },
+    { title: 'What does an orange taste like?', content: 'Like an orange!' },
+    { title: 'What does an orange taste like?', content: 'Like an orange!' },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.modalOpen = !this.modalOpen;
   }
-
 }
